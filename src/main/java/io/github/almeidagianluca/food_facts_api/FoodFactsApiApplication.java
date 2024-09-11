f09328d66cbcafa8e753bf4b4687cf461cca58b5
almeidagianluca <almeidagianluca@gmail.com>
2024-09-11 20:13:20 -0300

    Initial project structure

---
+++ src/main/java/io/github/almeidagianluca/food_facts_api/FoodFactsApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.almeidagianluca.food_facts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFactsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFactsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/almeidagianluca/food_facts_api/FoodFactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.almeidagianluca.food_facts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFactsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
