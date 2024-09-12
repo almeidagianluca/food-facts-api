b8725ed661a223894efe9139302bc61a4034d8b1
almeidagianluca <almeidagianluca@gmail.com>
2024-09-11 22:26:51 -0300

    create product model

---
+++ src/main/java/io/github/almeidagianluca/food_facts_api/model/Product.java
@@ -0,0 +1,22 @@
+package io.github.almeidagianluca.food_facts_api.model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "products")
+public class Product {
+    private Integer code;
+    private String status;
+    private String importedT;
+    private String url;
+    private String creator;
+    private long createdT;
+    private long lastModifiedT;
+    private String productName;
+    private String quantity;
+    private String brands;
+    private String categories;
+    private String labels;
+    private String cities;
+}
